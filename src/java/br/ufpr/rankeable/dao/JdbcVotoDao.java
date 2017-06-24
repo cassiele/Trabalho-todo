96e6a5a8ee3620bdde03265b67d1710adac7b410
cassiele <cassiele@web.ufpr.br>
2017-06-23 22:37:22 -0300

    Interface votoDao implements

--- src/java/br/ufpr/rankeable/dao/JdbcVotoDao.java
+++ src/java/br/ufpr/rankeable/dao/JdbcVotoDao.java
@@ -6,18 +6,24 @@
 package br.ufpr.rankeable.dao;
 
 import br.ufpr.rankeable.jdbc.MysqlConnectionFactory;
+import br.ufpr.rankeable.modelo.Voto;
 import java.sql.Connection;
 
 /**
  *
  * @author cassiele
  */
-public class JdbcVotoDao {
+public class JdbcVotoDao implements VotoDao {
     
     private Connection connection;
     
      public JdbcVotoDao() {
         connection = (new MysqlConnectionFactory()).getConnection();
+    }
+
+    @Override
+    public void adiciona(Voto voto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
      
      
