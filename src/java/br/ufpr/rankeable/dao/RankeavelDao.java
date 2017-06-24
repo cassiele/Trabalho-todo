d5ad81f16495792d0f6a5425ce487b917a08cccf
cassiele <cassiele@web.ufpr.br>
2017-06-23 22:09:38 -0300

    Criado classes Dao

---
+++ src/java/br/ufpr/rankeable/dao/RankeavelDao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufpr.rankeable.dao;
+
+import br.ufpr.rankeable.jdbc.MysqlConnectionFactory;
+import java.sql.Connection;
+
+/**
+ *
+ * @author cassiele
+ */
+public class RankeavelDao {
+    
+     private Connection connection;
+    
+     public RankeavelDao() {
+        connection = (new MysqlConnectionFactory()).getConnection();
+    }
+     
+     
+    
+}
---
+++ src/java/br/ufpr/rankeable/dao/RankingDao.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufpr.rankeable.dao;
+
+import br.ufpr.rankeable.jdbc.MysqlConnectionFactory;
+import java.sql.Connection;
+
+/**
+ *
+ * @author cassiele
+ */
+public class RankingDao {
+    
+     private Connection connection;
+    
+     public RankingDao() {
+        connection = (new MysqlConnectionFactory()).getConnection();
+    }
+     
+    
+}
